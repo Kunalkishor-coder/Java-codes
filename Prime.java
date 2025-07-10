53ddd013758b878f4b3fe30151d8ad5efe16df13
Kunal kishor <kunalkishor.kk.99@gmail.com>
2025-07-10 11:47:02 +0530

    first logics of to get prime and initial commit

---
+++ Prime.java
@@ -0,0 +1,20 @@
+public class Prime{
+	public static boolean isPrime(int data){
+		for(int i=2;i<data/2;i++){
+			if(data%i==0){
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	public static void main(String []args){
+		System.out.println("ENTER A NO. TO CHECK PRIME :");
+		int data = new Scanner(System.in);
+		if(isPrime(data))
+			System.out.println(data+" is a prime no.");
+		else
+			System.out.println(data+" is a not prime no.");
+		
+	}
+}
