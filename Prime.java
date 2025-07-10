cf9055f5e2015918ef410f95f5bd2580cfe74a5c
Kunal kishor <kunalkishor.kk.99@gmail.com>
2025-07-10 12:00:24 +0530

    second commit with .class file and more user friendly

--- Prime.java
+++ Prime.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Prime{
 	public static boolean isPrime(int data){
 		for(int i=2;i<data/2;i++){
@@ -9,12 +10,20 @@ public class Prime{
 		return true;
 	}
 	public static void main(String []args){
-		System.out.println("ENTER A NO. TO CHECK PRIME :");
-		int data = new Scanner(System.in);
-		if(isPrime(data))
-			System.out.println(data+" is a prime no.");
-		else
-			System.out.println(data+" is a not prime no.");
+		int n;
+		do{
+			if(n==5){
+				break;
+			}else{
+				System.out.println("ENTER A NO. TO CHECK PRIME :");
+				int data = new Scanner(System.in).nextInt();
+				if(isPrime(data))
+					System.out.println(data+" is a prime no.");
+				else
+					System.out.println(data+" is a not prime no.");
+			}
+		}
+		while(n!=5);
 		
 	}
 }
